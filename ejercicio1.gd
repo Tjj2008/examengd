e9231d6e26a21827074558fe58c0c9c49ae8882a
Aylen2005 <vabusto@escuelasproa.edu.ar>
2023-10-31 11:11:01 -0300

    hollla

---
+++ ejercicio1.gd
@@ -0,0 +1,10 @@
+extends Node
+var resultado=0
+func Sum_Odds([1,11,12,15,19]):
+    for numbers in ([1,11,12,15,19]):
+    	if Numbers / 2== 0:
+            pass
+            resultado += Numbers
+func _ready():
+	return
+    pass
---
+++ ejercicio2.gd
@@ -0,0 +1,23 @@
+extends Node
+var lista={}
+var inventory={
+    "name":"Julian"
+    
+    "surname":"Alvarez"
+    
+    "goals":"4"
+    
+    "teams":["Polonia","Australia","Croacia","Croacia]
+             }
+             
+func Julian_Alvarez(inventory):
+	for inventory in range(4)
+             lista += (["julian alvarez 4 goles"],
+                       ["Australia 1"],
+                       ["polonia 1"],
+                       ["Croacia 2"])
+		print lista:
+             
+func _ready():
+	return
+    pass
